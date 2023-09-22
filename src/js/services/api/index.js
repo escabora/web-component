5d38fa46a671d7c0fe8596cf2c5fae81016e15c9
escabora <gabrielescabora@gmail.com>
2023-09-22 14:27:08 -0300

    feat: apis and styles

--- src/js/services/api/index.js
+++ src/js/services/api/index.js
@@ -5,7 +5,16 @@ const delay = (data) =>
     }, 300)
   })
 
-const person = async () => {
+  const basic = async () => {
+    return await delay({
+      fileds: [],
+      titlePage: [],
+      nextPage: 'adress',
+      prevPage: 'person',
+    })
+  }
+
+  const person = async () => {
   return await delay({
     fileds: [],
     titlePage: [],
@@ -23,7 +32,28 @@ const adress = async () => {
   })
 }
 
+const profession = async () => {
+  return await delay({
+    fileds: [],
+    titlePage: [],
+    nextPage: 'adress',
+    prevPage: 'person',
+  })
+}
+
+const customization = async () => {
+  return await delay({
+    fileds: [],
+    titlePage: [],
+    nextPage: 'adress',
+    prevPage: 'person',
+  })
+}
+
 export default {
+  basic,
   person,
   adress,
+  profession,
+  customization,
 }
