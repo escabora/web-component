c2abc2590947364be323d2e60ea3cd0d56738458
escabora <gabrielescabora@gmail.com>
2023-09-22 11:00:42 -0300

    fix: ajust mixin

--- dist/js/index.js
+++ dist/js/index.js
@@ -103,9 +103,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
   !*** ./src/styles/index.scss ***!
   \*******************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: no mixin named force-show\\n        on line 3 of src/styles/utils/critical-helpers.scss\\n        from line 1 of src/styles/utils/index.scss\\n        from line 9 of /Users/gescabora/Documents/estudos/web-component/src/styles/index.scss\\n>>     @include force-show;\\n\\n   -------------^\\n\\n    at /Users/gescabora/Documents/estudos/web-component/node_modules/webpack/lib/NormalModule.js:316:20\\n    at /Users/gescabora/Documents/estudos/web-component/node_modules/loader-runner/lib/LoaderRunner.js:367:11\\n    at /Users/gescabora/Documents/estudos/web-component/node_modules/loader-runner/lib/LoaderRunner.js:233:18\\n    at context.callback (/Users/gescabora/Documents/estudos/web-component/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\\n    at Object.callback (/Users/gescabora/Documents/estudos/web-component/node_modules/sass-loader/dist/index.js:73:7)\\n    at Object.done [as callback] (/Users/gescabora/Documents/estudos/web-component/node_modules/neo-async/async.js:8069:18)\\n    at options.error (/Users/gescabora/Documents/estudos/web-component/node_modules/node-sass/lib/index.js:294:32)\");\n\n//# sourceURL=webpack:///./src/styles/index.scss?");
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/styles/index.scss?");
 
 /***/ }),
 
