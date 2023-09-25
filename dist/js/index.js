84efe82e46357dfce80ee2e92efd39e3b8b73a2a
escabora <gabrielescabora@gmail.com>
2023-09-25 09:32:03 -0300

    feat: insert ds in page

--- dist/js/index.js
+++ dist/js/index.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Progress__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Progress */ \"./src/js/components/Progress/index.js\");\n\n\nconst Form = props => {\n  return `\n  ${Object(_Progress__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(props)}\n  <div class=''>\n  <form><input /></form>\n  ${props.name}\n  </div>`;\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Form);\n\n//# sourceURL=webpack:///./src/js/components/Form/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Progress__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../Progress */ \"./src/js/components/Progress/index.js\");\n\n\nconst Form = props => {\n  return `\n  ${Object(_Progress__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(props)}\n  <div class=''>\n  <form><div class=\"col-desktop-12 col-tablet-12 col-mobile-12 form-field__wrapper\"><div class=\"ds-input\"><label class=\"ds-input__label\" for=\"ds-field-cpf\" id=\"ds-label-cpf\" aria-label=\"CPF\" aria-hidden=\"true\">CPF</label><div class=\"ds-input__field\"><input id=\"ds-field-cpf\" type=\"text\" aria-labelledby=\"ds-label-cpf\" aria-required=\"true\" aria-invalid=\"false\" autocomplete=\"on\" placeholder=\"000.000.000-00\" inputmode=\"numeric\" pattern=\"[0-9.-]*\" name=\"cpf\" value=\"429.144.368-40\"><div class=\"ds-input__icon ds-input__icon--right\" aria-hidden=\"true\"></div></div></div></div></form>\n  ${props.name}\n  </div>`;\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Form);\n\n//# sourceURL=webpack:///./src/js/components/Form/index.js?");
 
 /***/ }),
 
--- src/js/components/Form/index.js
+++ src/js/components/Form/index.js
@@ -1,9 +1,9 @@
-import Progress from "../Progress"
+import Progress from '../Progress'
 const Form = (props) => {
   return `
   ${Progress(props)}
   <div class=''>
-  <form><input /></form>
+  <form><div class="col-desktop-12 col-tablet-12 col-mobile-12 form-field__wrapper"><div class="ds-input"><label class="ds-input__label" for="ds-field-cpf" id="ds-label-cpf" aria-label="CPF" aria-hidden="true">CPF</label><div class="ds-input__field"><input id="ds-field-cpf" type="text" aria-labelledby="ds-label-cpf" aria-required="true" aria-invalid="false" autocomplete="on" placeholder="000.000.000-00" inputmode="numeric" pattern="[0-9.-]*" name="cpf" value="429.144.368-40"><div class="ds-input__icon ds-input__icon--right" aria-hidden="true"></div></div></div></div></form>
   ${props.name}
   </div>`
 }
