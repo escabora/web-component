eed936cf0820c0b8695b3db6bcdc703b23d74274
escabora <gabrielescabora@gmail.com>
2023-09-25 10:05:35 -0300

    feat: insert progress bar and style

--- src/js/components/Progress/index.js
+++ src/js/components/Progress/index.js
@@ -1,8 +1,6 @@
 const Progress = (props) => {
 
-  return `<div>
-    <h2>Progress ${props.name}</h2>
-  </div>`
+  return `<div class="ds-stepper"><div class="ds-stepper__content" role="text" aria-label="${props.name}, passo 1 de 4"><p class="ds-stepper__title" aria-hidden="true">${props.name}</p><p class="ds-stepper__state" aria-hidden="true">1 de 4</p></div><div class="ds-stepper__progress" aria-hidden="true"><div class="ds-stepper__bar" style="width: 25%;"></div></div></div>`
 
 }
 
