2431a2881f6972a791cf2dffc471d2c944527f39
escabora <gabrielescabora@gmail.com>
2023-09-25 09:48:14 -0300

    feat: insert styled and step one form init

--- src/js/core/index.js
+++ src/js/core/index.js
@@ -1,16 +1,19 @@
 import Layout from '../layout/index'
-
+import { slylePage } from './styled'
 export default function Blue(props) {
   return class extends HTMLElement {
     constructor() {
-      super();
-      const shaddow = this.attachShadow({mode: 'open'})
+      super()
+      const shaddow = this.attachShadow({ mode: 'open' })
+      const styleHead = document.createElement('style')
+      styleHead.textContent = slylePage
+      shaddow.appendChild(styleHead)
       this.render(shaddow, props)
     }
 
     render(shaddow, props) {
-      const section = document.createElement('section');
-      section.innerHTML = Layout(props);
+      const section = document.createElement('section')
+      section.innerHTML = Layout(props)
       shaddow.appendChild(section)
     }
   }
